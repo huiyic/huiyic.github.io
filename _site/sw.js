fcf14b43611955c13f97d2d962d42c1e9aa90e0b
Hui Yi <hui.ychia@gmail.com>
2018-07-30 01:45:42 +0800

    change service worker

--- _site/sw.js
+++ _site/sw.js
@@ -1,18 +1,37 @@
-var CACHE_NAME = 'huiyi-site';
+var CACHE = 'huiyi-site';
 var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
+  'https://huiyichia.com/assets/css/main.css',
+  'https://huiyichia.com/assets/css/noscript.css'
 ];
 
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(huiyi-site)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+// On install, cache some resources.
+self.addEventListener('install', function(evt) {
+      console.log('The service worker is being installed.');    
+    // Perform install steps
+  evt.waitUntil(precache());
 });
+    // On fetch, use cache only strategy.
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+});
+
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(urlsToCache);
+  });
+}
+    
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
