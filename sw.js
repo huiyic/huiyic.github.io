e3b5a56bc8738e0bb9703eceb5b1895b0d50b153
Hui Yi <hui.ychia@gmail.com>
2018-07-30 01:03:01 +0800

    define cache_name

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(huiyi-site)
+    caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
